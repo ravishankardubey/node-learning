e1eb799344bde4ed9c03825532004746bfe67e68
Ravi Shankar Dubey <ravi.dubey2@mindtree.com>
2019-09-16 14:30:11 +0530

    MVC Arch Module finished

---
+++ models/product.js
@@ -0,0 +1,34 @@
+const products = [];
+const fs = require('fs');
+const path = require('path');
+
+const rootDir = require('../util/path');
+const p = path.join(rootDir, 'data', 'products.json');
+
+const getProductsFromFile = cb => {
+    return fs.readFile(p, (err, fileContent) => {
+        if (err) {
+            return cb([]);
+        }
+        cb(JSON.parse(fileContent));
+    });
+}
+
+module.exports = class Product {
+    constructor(t) {
+        this.title = t;
+    }
+
+    save() {
+        getProductsFromFile(products => {
+            products.push(this);
+            fs.writeFile(p, JSON.stringify(products), (err) => {
+                console.error(err);
+            });
+        })
+    }
+
+    static fetchAll(cb) {
+        getProductsFromFile(cb);
+    }
+}
