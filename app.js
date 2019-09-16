758a3f14b5fb9e8c162919ca126909cc199e54bb
Ravi Shankar Dubey <ravi.dubey2@mindtree.com>
2019-09-16 12:31:36 +0530

    using ejs as templating engine for App

--- app.js
+++ app.js
@@ -1,22 +1,24 @@
-const express = require('express');
-const bodyParser = require('body-parser');
 const path = require('path');
 
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use(bodyParser.urlencoded({
-    extended: false
-}));
-app.use(express.static(path.join(__dirname,'public')));
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminData.routes);
 app.use(shopRoutes);
-app.use('/admin', adminRoutes);
 
-app.use((ewq, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
-})
+app.use((req, res, next) => {
+  res.status(404).render('404', { pageTitle: 'Page Not Found' });
+});
 
 app.listen(3000);
